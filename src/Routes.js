db1f0a3265ca1da87d92fbbec7049cfd66deaf36
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 01:56:09 +0530

    Wrapping it in ThemeProvider & NavigationLayout, Adding ModalRouter

--- src/Routes.js
+++ src/Routes.js
@@ -1,7 +1,31 @@
+// IMPORTING MODULES/PACKAGES
+import { ThemeProvider } from "@mui/material/styles";
+
 import React from "react";
+import { useRecoilState } from "recoil";
+import { DarkModeAtom } from "./Context/atoms";
+import { DarkMode, LightMode } from "./Style/Themes";
+import { CssBaseline } from "@mui/material";
+import { NavigationLayout } from "./Layout/NavigationLayout/NavigationLayout";
+import ModalRouter from "./Component/ModalRouter/ModalRouter";
 
 const Routes = () => {
-  return <></>;
+  // GETTING ATOMIC STATES
+  const [isDarkMode] = useRecoilState(DarkModeAtom);
+
+  return (
+    <ThemeProvider theme={isDarkMode ? DarkMode : LightMode}>
+      <CssBaseline />
+      <ModalRouter />
+      <NavigationLayout>
+        loIncididunt deserunt do id voluptate fugiat occaecat fugiat nisi non
+        duis amet amet voluptate labore. Quis sit ad veniam irure consectetur
+        sit voluptate fugiat aute nostrud nisi sunt. Veniam reprehenderit aliqua
+        officia aliqua mollit ut est. Incididunt enim do culpa cupidatat
+        exercitation est eu.
+      </NavigationLayout>
+    </ThemeProvider>
+  );
 };
 
 export default Routes;
