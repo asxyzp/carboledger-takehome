a87778f061168fd7a41fd84a778d8ef65abb21c0
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 03:16:30 +0530

    Adding SheetsCell component

---
+++ src/Cell/SheetsCell.js
@@ -0,0 +1,33 @@
+import { NoteAdd } from "@mui/icons-material";
+import { Card, Typography } from "@mui/material";
+import React from "react";
+
+const Empty = () => {
+  return (
+    <Card
+      sx={{
+        "&.MuiCard-root": {
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px 10px",
+        },
+      }}
+    >
+      <NoteAdd color="primary" sx={{ fontSize: "5em" }} />
+      <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+        No sheets found
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "center" }}>
+        Please upload the spreadsheet file (.xlsx)
+      </Typography>
+    </Card>
+  );
+};
+
+const SheetsCell = () => {
+  return <Empty />;
+};
+
+export default SheetsCell;
