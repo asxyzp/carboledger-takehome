ba18668bf542c5bc7eab50bbf6a758117d7aa4d2
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 03:44:21 +0530

    Updating default border-radius of card component

--- src/Component/Card/Card.js
+++ src/Component/Card/Card.js
@@ -2,7 +2,7 @@ import { Card as MuiCard, styled } from "@mui/material";
 
 const Card = styled(MuiCard)(({ theme }) => ({
   "&.MuiCard-root": {
-    borderRadius: "10px",
+    borderRadius: "15px",
     padding: "10px",
   },
 }));
