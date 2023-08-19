e929023989f8dfd2181e0239876af6c4796cf8c6
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 03:50:13 +0530

    Updating loading state container

--- src/Cell/SheetsCell.js
+++ src/Cell/SheetsCell.js
@@ -1,6 +1,7 @@
 import { Card, Typography, useMediaQuery } from "@mui/material";
 import React from "react";
 import EmptyImg from "../Assets/empty.svg";
+import LoadingImg from "../Assets/loading.svg";
 
 const Empty = () => {
   // SETTING MEDIA QUERY
@@ -17,19 +18,52 @@ const Empty = () => {
           padding: "50px 10px",
           borderRadius: "15px",
         },
-        "& .empty-sheet-img": {
+        "& .sheet-cell-img": {
           width: isMobileMode ? "50%" : "20%",
           marginBottom: "10px",
         },
       }}
       elevation={3}
     >
-      <img src={EmptyImg} alt="Empty" className="empty-sheet-img" />
+      <img src={EmptyImg} alt="Empty" className="sheet-cell-img" />
       <Typography variant="h5" sx={{ fontWeight: "bold" }}>
         No sheets found
       </Typography>
       <Typography variant="body2" sx={{ textAlign: "center" }}>
         Please upload the spreadsheet file (.xlsx)
+      </Typography>
+    </Card>
+  );
+};
+
+const Loading = () => {
+  // SETTING MEDIA QUERY
+  const isMobileMode = useMediaQuery("(max-width: 650px)");
+
+  return (
+    <Card
+      sx={{
+        "&.MuiCard-root": {
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px 10px",
+          borderRadius: "15px",
+        },
+        "& .sheet-cell-img": {
+          width: isMobileMode ? "50%" : "20%",
+          marginBottom: "15px",
+        },
+      }}
+      elevation={3}
+    >
+      <img src={LoadingImg} alt="Empty" className="sheet-cell-img" />
+      <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+        Looking for sheets
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "center" }}>
+        Searching through the database
       </Typography>
     </Card>
   );
