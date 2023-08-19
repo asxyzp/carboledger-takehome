c429109bbe1f3c62c6ecc972b72e14db2bfd14f9
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 01:49:36 +0530

    Adding ModalType atom to atoms.js

---
+++ src/Context/atoms.js
@@ -0,0 +1,14 @@
+// IMPORTING MODULES/PACKAGES
+import { atom } from "recoil";
+
+// STORING DARK MODE INFORMATION
+export const DarkModeAtom = atom({
+  key: "darkModeAtom",
+  default: false,
+});
+
+// STORING MODAL TYPE INFORMATION
+export const ModalTypeAtom = atom({
+  key: "modalModeAtom",
+  default: "",
+});
