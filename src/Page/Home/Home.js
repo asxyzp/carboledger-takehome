b63781447b37dec3d06eb82e4f10477e9efbaa56
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 03:16:52 +0530

    Updating home component

---
+++ src/Page/Home/Home.js
@@ -0,0 +1,35 @@
+// IMPORTING PACKAGES/MODULES
+import { Box, Divider, Typography } from "@mui/material";
+import React from "react";
+import Button from "../../Component/Button/Button";
+import { BackupTable, NoteAdd } from "@mui/icons-material";
+import "./home.css";
+import Card from "../../Component/Card/Card";
+import SheetsCell from "../../Cell/SheetsCell";
+
+const Home = () => {
+  return (
+    <Box className="home-page">
+      <Typography className="home-page-title">
+        Upload the relevant spreadsheet file and explore the carbon emission
+        data
+      </Typography>
+
+      <Box className="home-page-sheets-upload-container">
+        <Box className="home-page-sheets-upload-icon">
+          <BackupTable color="primary" />
+          <Typography variant="h6" sx={{ ml: "5px" }}>
+            Sheets
+          </Typography>
+        </Box>
+        <Button variant="contained" color="primary" size="xs">
+          Upload (.xlsx)
+        </Button>
+      </Box>
+
+      <SheetsCell />
+    </Box>
+  );
+};
+
+export default Home;
