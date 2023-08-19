2c75a4410da0581be7597cd6c701ff8daf7644a4
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 01:44:47 +0530

    Installing Recoil.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Routes from "./Routes";
+import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Routes />
+    <RecoilRoot>
+      <Routes />
+    </RecoilRoot>
   </React.StrictMode>
 );
