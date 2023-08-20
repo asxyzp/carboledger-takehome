e7f416874e581638385bdd615e91e65eef893027
n00bgineer <n00bgineer@protonmail.com>
2023-08-20 12:10:01 +0530

    Updating state components in SheetsCell

--- src/Cell/SheetsCell.js
+++ src/Cell/SheetsCell.js
@@ -1,12 +1,12 @@
-import { Card, Typography, useMediaQuery } from "@mui/material";
+// IMPORTING PACKAGES/MODULES
+import { Card, Skeleton, Typography } from "@mui/material";
 import React from "react";
-import EmptyImg from "../Assets/empty.svg";
-// import LoadingImg from "../Assets/loading.svg";
+import EmptyImg from "../Asset/empty.svg";
+import ErrorImg from "../Asset/error.svg";
 
-const Empty = () => {
-  // SETTING MEDIA QUERY
-  const isMobileMode = useMediaQuery("(max-width: 650px)");
-
+// CUSTOM COMPONENTS
+// CUSTOM CARD COMPONENT
+const CustomCard = ({ children }) => {
   return (
     <Card
       sx={{
@@ -19,58 +19,63 @@ const Empty = () => {
           borderRadius: "15px",
         },
         "& .sheet-cell-img": {
-          width: isMobileMode ? "50%" : "20%",
+          width: "250px",
           marginBottom: "10px",
         },
       }}
       elevation={3}
     >
+      {children}
+    </Card>
+  );
+};
+
+// STATE COMPONENTS
+// EMPTY STATE COMPONENT
+const Empty = () => {
+  return (
+    <CustomCard>
       <img src={EmptyImg} alt="Empty" className="sheet-cell-img" />
-      <Typography variant="h5" sx={{ fontWeight: "bold" }}>
+      <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "center" }}>
         No sheets found
       </Typography>
       <Typography variant="body2" sx={{ textAlign: "center" }}>
         Please upload the spreadsheet file (.xlsx)
       </Typography>
-    </Card>
+    </CustomCard>
   );
 };
 
-// const Loading = () => {
-//   // SETTING MEDIA QUERY
-//   const isMobileMode = useMediaQuery("(max-width: 650px)");
+// LOADING STATE COMPONENT
+const Loading = () => {
+  return (
+    <Skeleton
+      variant="rectangular"
+      sx={{
+        borderRadius: "15px",
+        height: "330px",
+      }}
+    />
+  );
+};
 
-//   return (
-//     <Card
-//       sx={{
-//         "&.MuiCard-root": {
-//           display: "flex",
-//           flexDirection: "column",
-//           justifyContent: "center",
-//           alignItems: "center",
-//           padding: "50px 10px",
-//           borderRadius: "15px",
-//         },
-//         "& .sheet-cell-img": {
-//           width: isMobileMode ? "50%" : "20%",
-//           marginBottom: "15px",
-//         },
-//       }}
-//       elevation={3}
-//     >
-//       <img src={LoadingImg} alt="Empty" className="sheet-cell-img" />
-//       <Typography variant="h5" sx={{ fontWeight: "bold" }}>
-//         Looking for sheets
-//       </Typography>
-//       <Typography variant="body2" sx={{ textAlign: "center" }}>
-//         Searching through the database
-//       </Typography>
-//     </Card>
-//   );
-// };
+// ERROR STATE COMPONENT
+const Error = () => {
+  return (
+    <CustomCard>
+      <img src={ErrorImg} alt="Empty" className="sheet-cell-img" />
+      <Typography variant="h5" sx={{ fontWeight: "bold", textAlign: "center" }}>
+        Something went wrong
+      </Typography>
+      <Typography variant="body2" sx={{ textAlign: "center" }}>
+        Apparently we can't load the sheets
+      </Typography>
+    </CustomCard>
+  );
+};
 
 const SheetsCell = () => {
-  return <Empty />;
+  return <Error />;
 };
 
 export default SheetsCell;
