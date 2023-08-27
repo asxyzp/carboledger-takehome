bbc4f095fa2ba59e8528cf833ec4bdcdf735362d
--global <--global>
2023-08-27 16:08:42 +0530

    Updating sheets component

--- src/Component/Sheets/Sheets/Sheets.js
+++ src/Component/Sheets/Sheets/Sheets.js
@@ -1,8 +1,31 @@
 // IMPORTING MODULES/PACKAGES
 import React from "react";
+import EmptyImg from "../../../Asset/empty.svg";
+import { StateCard } from "../../StateCard/StateCard";
+import { Typography } from "@mui/material";
+
+// CUSTOM COMPONENTS
+// EMPTY SHEETS COMPONENT
+export const Empty = () => {
+  return (
+    <StateCard>
+      <img src={EmptyImg} alt="Empty" className="state-card-image" />
+      <Typography variant="h4" className="state-card-title">
+        No Sheets found
+      </Typography>
+      <Typography variant="body2" className="state-card-subtitle">
+        Please upload a sheet to continue
+      </Typography>
+    </StateCard>
+  );
+};
 
 const Sheets = ({ sheets }) => {
-  return <div>Sheets</div>;
+  if (sheets.length === 0) {
+    return <Empty />;
+  } else {
+    return <>Non-empty</>;
+  }
 };
 
 export default Sheets;
