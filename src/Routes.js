48aeb6b2a4620cb73f21f1a7e99742fe27aac00b
--global <--global>
2023-08-27 13:46:48 +0530

    Adding /sheets and /sheet/:id page along with routing to Routes.js

--- src/Routes.js
+++ src/Routes.js
@@ -5,13 +5,80 @@ import React from "react";
 import { useRecoilState } from "recoil";
 import { DarkModeAtom } from "./Context/atoms";
 import { DarkMode, LightMode } from "./Style/Themes";
-import { CssBaseline } from "@mui/material";
+import { CssBaseline, Skeleton, Typography } from "@mui/material";
 import { NavigationLayout } from "./Layout/NavigationLayout/NavigationLayout";
 import ModalRouter from "./Component/ModalRouter/ModalRouter";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./Page/HomePage/HomePage";
 import SheetsPage from "./Page/SheetsPage/SheetsPage";
 import SheetPage from "./Page/SheetPage/SheetPage";
+import { StateCard } from "./Component/StateCard/StateCard";
+import ErrorImg from "./Asset/error.svg";
+
+// LOADER METHODS
+/**
+ * @name getSheets
+ * @description METHOD TO LOAD SHEETS DATA
+ * @returns {Object} SheetsData
+ */
+const getSheets = async () => {
+  return await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve([1, 2, 3, 4]);
+    }, 500)
+  );
+};
+
+/**
+ * @name getSheet
+ * @description METHOD TO LOAD SHEET DATA
+ * @returns {Object} SheetData
+ */
+const getSheet = async (id) => {
+  return await new Promise((resolve) =>
+    setTimeout(() => {
+      resolve([4, 3, 2, 1]);
+    }, 500)
+  );
+};
+
+// CUSTOM COMPONENTS
+// ERROR COMPONENT
+export const Error = () => {
+  return (
+    <StateCard>
+      <img src={ErrorImg} alt="Empty" className="sheet-cell-img" />
+      <Typography variant="h4" sx={{ fontWeight: "bold", textAlign: "center" }}>
+        Whoops!
+      </Typography>
+      <Typography
+        variant="body2"
+        sx={{ textAlign: "center", width: "60%", mt: "5px" }}
+      >
+        Something went wrong. We couldn't load your data.
+      </Typography>
+    </StateCard>
+  );
+};
+
+// LOADING COMPONENT
+export const Loading = () => {
+  return (
+    <Skeleton
+      variant="rectangular"
+      sx={(theme) => {
+        return {
+          "&.MuiSkeleton-root": {
+            padding: "50px 10px",
+            borderRadius: "15px",
+            minHeight: "75vh",
+            border: `1px solid ${theme.palette.divider}`,
+          },
+        };
+      }}
+    />
+  );
+};
 
 const Routes = () => {
   // GETTING ATOMIC STATES
@@ -26,16 +93,14 @@ const Routes = () => {
     {
       path: "/sheets",
       element: <SheetsPage />,
-      loader: () => {
-        return { a: "a" };
-      },
+      loader: () => getSheets(),
+      errorElement: <Error />,
     },
     {
       path: "/sheets/:sheet_id",
       element: <SheetPage />,
-      loader: () => {
-        return { b: "b" };
-      },
+      loader: () => getSheet(),
+      errorElement: <Error />,
     },
   ]);
 
@@ -44,7 +109,7 @@ const Routes = () => {
       <CssBaseline />
       <ModalRouter />
       <NavigationLayout>
-        <RouterProvider router={router} />
+        <RouterProvider router={router} fallbackElement={<Loading />} />
       </NavigationLayout>
     </ThemeProvider>
   );
